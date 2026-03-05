d87de11d72522712684fd7a9982b14b2a62c5ba1
GabrieIPujol <gabriel.pagvelosa@gmail.com>
2026-03-05 12:00:43 -0300

    Finished the Story section!

--- src/components/AnimatedTitle.jsx
+++ src/components/AnimatedTitle.jsx
@@ -1,9 +1,12 @@
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import clsx from "clsx";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const AnimatedTitle = ({ title, containerClass }) => {
-  
+
     // Declarando a referencia do container para o ScrollTrigger
     const containerRef = useRef(null);
 
@@ -24,22 +27,22 @@ const AnimatedTitle = ({ title, containerClass }) => {
                 transform: 'translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)',
                 ease: 'power2.inOut',
                 stagger: 0.02,
-            })
+            }, 0);
         }, containerRef)
         return () => ctx.revert();
     }, []);
-  
+
     return (
-    <div ref={containerRef} className={`animated-title ${containerClass}`}>
-        {title.split('<br/>').map((line, index) => (
-            <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
-                {line.split(' ').map((word, i) => (
-                    <span key={i} className="animated-word" dangerouslySetInnerHTML={{__html: word}}></span>
-                ))}
-            </div>
-        ))}
-    </div>
-  )
+        <div ref={containerRef} className={clsx('animated-title', containerClass)}>
+            {title.split('<br/>').map((line, index) => (
+                <div key={index} className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3">
+                    {line.split(" ").map((word, idx) => (
+                        <span key={idx} className="animated-word" dangerouslySetInnerHTML={{ __html: word }}></span>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default AnimatedTitle
